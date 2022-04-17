46c0379cfbe23b49477ffbfdc7518f6e7acc3ae1
Sudip Mishra <popeye.hitk@gmail.com>
2022-04-17 20:51:47 +0530

    todo_app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Todo from "./components/Todo";
+
+function App() {
+  return  (
+  <div>
+    <h1>My Todos</h1>
+    <Todo text = "learn react" />
+    <Todo text ="master react"/>
+    <Todo text = "explore the full react course"/>
+  </div>
+  );
+}
+
+export default App;
